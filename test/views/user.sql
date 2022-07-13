2613075cc657199a34abe945a506a98ca779313d
Max <79068976350@yandex.ru>
2022-07-13 17:38:13 +0400

    refactor test

--- test/views/user.sql
+++ test/views/user.sql
@@ -9,7 +9,7 @@ CREATE TABLE "user_trans"
     "title" VARCHAR(255),
     PRIMARY KEY ("id", "lang")
 ) INHERITS ("lang_base_tran");
-CALL create_user_view('v_user'::TEXT, '"user"'::REGCLASS, 'user_trans'::REGCLASS);
+CALL create_user_view(NULL::TEXT, '"user"'::REGCLASS, 'user_trans'::REGCLASS);
 
 SELECT *
 FROM "v_user";
