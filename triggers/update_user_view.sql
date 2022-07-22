e40132db2e757f04485d920d4dec6c6e09e323b2
Max <79068976350@yandex.ru>
2022-07-22 20:39:08 +0400

    fix new get_columns in triggers/*_user_view

--- triggers/update_user_view.sql
+++ triggers/update_user_view.sql
@@ -8,12 +8,12 @@ DECLARE
     "lb_table"       CONSTANT REGCLASS NOT NULL = TG_ARGV[0];
     "lb_pk_columns"  CONSTANT TEXT[] NOT NULL   = get_primary_key("lb_table");
     "lb_pk_values"            TEXT[];
-    "lb_columns"     CONSTANT TEXT[] NOT NULL   = get_columns("lb_table");
+    "lb_columns"     CONSTANT TEXT[] NOT NULL   = get_columns("lb_table", FALSE);
     "lb_values"               TEXT[];
     "lbt_table"      CONSTANT REGCLASS NOT NULL = TG_ARGV[1];
     "lbt_pk_columns" CONSTANT TEXT[] NOT NULL   = get_primary_key("lbt_table");
     "lbt_pk_values"           TEXT[];
-    "lbt_columns"    CONSTANT TEXT[] NOT NULL   = get_columns("lbt_table");
+    "lbt_columns"    CONSTANT TEXT[] NOT NULL   = get_columns("lbt_table", FALSE);
     "lbt_values"              TEXT[];
     "column"                  TEXT;
 BEGIN
