bd756381d68b8a59d2decfbbf7c002c0bacbaf08
Max <79068976350@yandex.ru>
2022-08-18 22:02:27 +0400

    fix name rule lang_base_tran__insert

--- tables/lang_base_tran.sql
+++ tables/lang_base_tran.sql
@@ -2,7 +2,7 @@ CREATE TABLE "lang_base_tran" (
     "lang" @extschema@.LANG NOT NULL REFERENCES "langs" ("lang") ON UPDATE CASCADE
 );
 
-CREATE RULE "lang_base_trans__insert" AS ON INSERT TO "lang_base_tran"
+CREATE RULE "lang_base_tran__insert" AS ON INSERT TO "lang_base_tran"
     DO INSTEAD
     NOTHING;
 
