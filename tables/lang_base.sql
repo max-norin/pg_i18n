3029a07dbdf7d7c5e4b4a69c0be2fb5c684f7d62
max_norin <79068976350@yandex.ru>
2024-05-08 16:59:15 +0300

    add rules

--- tables/lang_base.sql
+++ tables/lang_base.sql
@@ -2,7 +2,7 @@ CREATE TABLE "lang_base" (
     "default_lang" @extschema@.LANG NOT NULL REFERENCES "langs" ("lang") ON UPDATE CASCADE
 );
 
-CREATE RULE "lang_base__insert" AS ON INSERT TO "lang_base"
-    DO INSTEAD
-    NOTHING;
+CREATE RULE "insert" AS ON INSERT TO "lang_base" DO INSTEAD NOTHING;
+CREATE RULE "update" AS ON UPDATE TO "lang_base" DO INSTEAD NOTHING;
+CREATE RULE "delete" AS ON DELETE TO "lang_base" DO INSTEAD NOTHING;
 
--- tables/lang_base_tran.sql
+++ tables/lang_base_tran.sql
@@ -2,7 +2,6 @@ CREATE TABLE "lang_base_tran" (
     "lang" @extschema@.LANG NOT NULL REFERENCES "langs" ("lang") ON UPDATE CASCADE
 );
 
-CREATE RULE "lang_base_tran__insert" AS ON INSERT TO "lang_base_tran"
-    DO INSTEAD
-    NOTHING;
-
+CREATE RULE "insert" AS ON INSERT TO "lang_base_tran" DO INSTEAD NOTHING;
+CREATE RULE "update" AS ON UPDATE TO "lang_base_tran" DO INSTEAD NOTHING;
+CREATE RULE "delete" AS ON DELETE TO "lang_base_tran" DO INSTEAD NOTHING;
