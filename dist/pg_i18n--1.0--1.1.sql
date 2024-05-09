431ab3fccc4b90ab31e2b1924d715ac1e180391b
max_norin <79068976350@yandex.ru>
2024-05-09 19:51:04 +0300

    add comment and optimize

--- dist/pg_i18n--1.0--1.1.sql
+++ dist/pg_i18n--1.0--1.1.sql
@@ -24,6 +24,55 @@ COMMENT ON FUNCTION  @extschema@.get_columns (OID, BOOLEAN, TEXT) IS 'get table 
 DROP FUNCTION  @extschema@.get_columns ("relid" OID, "has_generated_column" BOOLEAN, "rel" TEXT);
 
 /*
+=================== UPDATE_USING_RECORDS ===================
+*/
+CREATE OR REPLACE FUNCTION update_using_records ("table" REGCLASS, "ch_columns" TEXT[], "old" RECORD, "new" RECORD)
+    RETURNS JSONB
+AS $$
+DECLARE
+    -- pk  - primary key
+    -- ch  - changed
+    -- main
+    "result"                     JSONB  NOT NULL = '{}';
+    -- table
+    "columns"           CONSTANT TEXT[] NOT NULL = @extschema@.get_columns("table", FALSE);
+    -- primary keys
+    "pk_columns"        CONSTANT TEXT[] NOT NULL = @extschema@.get_primary_key("table");
+    "pk_values"                  TEXT[];
+    -- changed values
+    "ch_columns"        CONSTANT TEXT[] NOT NULL = "columns" OPERATOR ( @extschema@.& ) "ch_columns";
+    "ch_values"                  TEXT[];
+    -- helpers
+    "column"                     TEXT;
+BEGIN
+    IF array_length("ch_columns", 1) IS NOT NULL THEN
+        -- set primary key values
+        FOREACH "column" IN ARRAY "pk_columns" LOOP
+                "pk_values" = array_append("pk_values", format('$1.%I', "column"));
+            END LOOP;
+        -- set changed values
+        FOREACH "column" IN ARRAY "ch_columns" LOOP
+                "ch_values" = array_append("ch_values", format('$2.%I', "column"));
+            END LOOP;
+        -- update and return record from table
+        EXECUTE format('UPDATE %1s SET (%2s)=ROW(%3s) WHERE (%4s)=(%5s) RETURNING to_json(%6s.*);', "table", "ch_columns", "ch_values", "pk_columns", "pk_values", "table")
+            INTO "result" USING "old", "new";
+    END IF;
+
+    RETURN "result";
+END
+$$
+    LANGUAGE plpgsql
+    VOLATILE
+    SECURITY DEFINER
+    RETURNS NULL ON NULL INPUT;
+
+/*
+=================== UPDATE_USING_ARRAYS ===================
+*/
+DROP FUNCTION update_using_arrays ("table" REGCLASS, "pk_columns" TEXT[], "pk_values" TEXT[], "ch_columns" TEXT[], "ch_values" TEXT[], "old" RECORD, "new" RECORD);
+
+/*
 =================== LANG_BASE ===================
 */
 CREATE RULE "update" AS ON UPDATE TO @extschema@."lang_base" DO INSTEAD NOTHING;
