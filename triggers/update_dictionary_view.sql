d4c63dffe2ba25bddafbcb54a220777595c900c1
max_norin <79068976350@yandex.ru>
2024-05-09 19:06:32 +0300

    add comment

--- triggers/update_dictionary_view.sql
+++ triggers/update_dictionary_view.sql
@@ -1,3 +1,4 @@
+-- триггер для обновления данных в словарном представлении
 CREATE FUNCTION trigger_update_dictionary_view ()
     RETURNS TRIGGER
     AS $$
@@ -6,10 +7,12 @@ DECLARE
     "lbt_record"              JSONB    NOT NULL = '{}';
     "lbt_table"      CONSTANT REGCLASS NOT NULL = TG_ARGV[1];
     "lbt_pk_columns" CONSTANT TEXT[]   NOT NULL = @extschema@.get_primary_key("lbt_table");
+    -- все колонки кроме primary_key (secondary key)
     "lbt_sk_columns" CONSTANT TEXT[]   NOT NULL = @extschema@.get_columns("lbt_table", FALSE) OPERATOR ( @extschema@.- ) "lbt_pk_columns";
     -- lang base
     "lb_record"               JSONB    NOT NULL = '{}';
     "lb_table"       CONSTANT REGCLASS NOT NULL = TG_ARGV[0];
+    -- все колонки lb_table отличные от колонок в таблице lbt_table
     "lb_ch_columns"  CONSTANT TEXT[]   NOT NULL = @extschema@.get_columns("lb_table", FALSE) OPERATOR ( @extschema@.- ) "lbt_sk_columns";
 BEGIN
     -- insert or update and return record from lb_table
@@ -18,6 +21,12 @@ BEGIN
     "lbt_record" = @extschema@.insert_or_update_using_records("lbt_table", NEW);
 
     -- change NEW using changed records
+    -- Используется, чтобы при испольовании RETURNING вернулся объект,
+    -- который будет соответствовать данным в таблицах,
+    -- это используется, так как данные из NEW могут не примениться или быть изменены триггерами.
+    -- jsonb_populate_record(base anyelement, from_json jsonb) -
+    -- Разворачивает объект из from_json в табличную строку,
+    -- в которой столбцы соответствуют типу строки, заданному параметром base.
     NEW = jsonb_populate_record(NEW, "lb_record");
     NEW = jsonb_populate_record(NEW, "lbt_record");
 
@@ -25,6 +34,6 @@ BEGIN
 END
 $$
     LANGUAGE plpgsql
-    VOLATILE
-    SECURITY DEFINER;
+    VOLATILE -- может делать всё, что угодно, в том числе, модифицировать базу данных
+    SECURITY DEFINER;  -- функция выполняется с правами пользователя, владеющего ей
 
