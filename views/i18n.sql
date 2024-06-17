4ed9d530208079d86ff9849e3cff35dd83d6c013
max_norin <79068976350@yandex.ru>
2024-06-17 22:51:19 +0300

    refactor and doc

--- views/i18n.sql
+++ views/i18n.sql
@@ -3,11 +3,11 @@
 CREATE OR REPLACE PROCEDURE public.create_i18n_view ("baserel" OID, "tranrel" OID)
     AS $$
 DECLARE
-    "pk_columns"     CONSTANT TEXT[] = public.get_primary_key("baserel");
-    -- массив для составления выражения JOIN ON <...>
-    "pk_columns_on"           TEXT[] = '{}';
-    "base_columns"   CONSTANT TEXT[] = public.get_columns("baserel");
-    "tran_columns"   CONSTANT TEXT[] = public.get_columns("tranrel");
+    "pk_columns"    CONSTANT TEXT[] = public.get_primary_key("baserel");
+    -- массив для составления выражения JOIN ON <...> с primary_key, нет возможности использовать USING
+    "pk_join_on"             TEXT[] = '{}';
+    "base_columns"  CONSTANT TEXT[] = public.get_columns("baserel");
+    "tran_columns"  CONSTANT TEXT[] = public.get_columns("tranrel");
     "column"                 TEXT;
     "name"                   TEXT;
     "select"                 TEXT[] = '{}';
@@ -20,54 +20,62 @@ BEGIN
 
     -- далее b - базовая таблица, t - таблица переводов
 
+    -- установка выражения сравнения для JOIN ON <...>
+    FOREACH "column" IN ARRAY "pk_columns" LOOP
+        "pk_join_on" = array_append("pk_join_on", format('b.%1$I = t.%1$I', "column"));
+    END LOOP;
+
+    -- установка имени представления
     "name" = 'v_dictionary_default';
+
     -- установка select
-    -- добавление колонок базовой таблицы, включая первичные ключи, но без повторяющихся колонок таблицы переводов
+    -- добавление колонок базовой таблицы, включая первичные ключи, но без одноименных колонок таблицы переводов
     FOREACH "column" IN ARRAY "pk_columns" || ("base_columns" OPERATOR ( public.- ) "tran_columns") LOOP
         "select" = array_append("select", format('b.%1I', "column"));
     END LOOP;
     -- добавление колонок из таблицы переводов
     FOREACH "column" IN ARRAY "tran_columns" OPERATOR ( public.- ) '{lang}'::TEXT[] OPERATOR ( public.- ) "pk_columns" LOOP
-        -- если колонка column есть среди колонок таблицы baserel, то использовать особую вставку с использованием CASE
-        IF "column" = ANY ("base_columns") THEN
-            "select" = array_append("select", format('CASE WHEN (t.*) IS NULL THEN b.%1$I ELSE t.%1$I END AS %1$I', "column"));
-        ELSE
-            "select" = array_append("select", format('t.%1I', "column"));
-        END IF;
-    END LOOP;
-    -- установить выражение сравнения для JOIN ON <...>
-    FOREACH "column" IN ARRAY "pk_columns" LOOP
-        "pk_columns_on" = array_append("pk_columns_on", format('b.%1$I = t.%1$I', "column"));
+        -- если колонка есть среди колонок таблицы baserel, то использовать особую вставку CASE
+        "select" = array_append("select", CASE WHEN "column" = ANY ("base_columns")
+            THEN format('CASE WHEN (t.*) IS NULL THEN b.%1$I ELSE t.%1$I END AS %1$I', "column")
+            ELSE format('t.%1I', "column") END);
     END LOOP;
 
-    -- создание представления
+    -- создание представления с записями языка по-умолчанию
     -- %s - вставляется как простая строка
     -- %I - вставляется как идентификатора SQL, экранируется при необходимости
-    "query" = format('SELECT %1s FROM %2I b LEFT JOIN %3I t ON %4s AND b.default_lang = t.lang WHERE TRUE',
+    -- LEFT JOIN tranrel - соединяем с имеющимися переводами
+    "query" = format('SELECT %1s FROM %2I b LEFT JOIN %3I t ON %4s AND b."default_lang" = t."lang"',
                    array_to_string("select", ','),
                    "baserel"::REGCLASS,
                    "tranrel"::REGCLASS,
-                   array_to_string("pk_columns_on", ' AND '));
-    RAISE NOTICE USING MESSAGE = "query";
+                   array_to_string("pk_join_on", ' AND '));
     EXECUTE format('CREATE VIEW %1I AS %2s;', "name", "query");
 
+    -- далее b - таблица дефолтных значений, t - таблица переводов, l - таблица языков
+
+    -- установка имени представления
     "name" = 'v_dictionary';
+
     -- установка select
-    -- свойство lang из таблицы langs, используется из объединения CROSS JOIN "langs"
-    "select" = array_prepend('"langs"."lang"', "select");
-    "select" = array_prepend('(b.default_lang = langs.lang) IS TRUE AS is_default_lang', "select");
+    -- колонка - lang из таблицы langs, используется из объединения CROSS JOIN "langs"
+    "select" = array_prepend('l."lang"', "select");
+    -- колонка - запись с дефолтным языком
+    "select" = array_prepend('(b."default_lang" = l."lang") IS TRUE AS "is_default_lang"', "select");
+    -- колонка - является переводом
     "select" = array_prepend('NOT ((t.*) IS NULL) AS "is_tran"', "select");
 
-    -- создание представления
+    -- создание представления с записями по всем языкам
     -- %s - вставляется как простая строка
     -- %I - вставляется как идентификатора SQL, экранируется при необходимости
-    -- CROSS JOIN "langs", чтобы в представлении были указанны все языки из таблицы "langs"
-    "query" = format('WITH b AS (%1s) SELECT %2s FROM b CROSS JOIN public."langs" LEFT JOIN %3I t ON %4s AND langs.lang = t.lang',
-                     "query",
+    -- WITH - общее табличное выражение с представлением из предыдущего запроса
+    -- CROSS JOIN "langs" - соединение значения со всеми языками
+    -- LEFT JOIN tranrel - соединение с имеющимися переводами
+    "query" = format('WITH b AS (%1s) SELECT %2s FROM b CROSS JOIN public."langs" l LEFT JOIN %3I t ON %4s AND l."lang" = t."lang"',
+                     "query", -- предыдущий запрос
                      array_to_string("select", ','),
                      "tranrel"::REGCLASS,
-                     array_to_string("pk_columns_on", ' AND '));
-    RAISE NOTICE USING MESSAGE = "query";
+                     array_to_string("pk_join_on", ' AND '));
     EXECUTE format('CREATE VIEW %1I AS %2s;', "name", "query");
 END
 $$
