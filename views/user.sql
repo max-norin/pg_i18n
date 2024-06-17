3d759a936c9a7719e78beea78372b50d7f365f33
max_norin <79068976350@yandex.ru>
2024-06-17 17:57:22 +0300

    new user

--- views/user.sql
+++ views/user.sql
@@ -1,31 +1,25 @@
 -- создание представления для пользовательского способа
 -- представление не только отображает данные, но даёт возможность редактирования
-CREATE PROCEDURE public.create_user_view ("name" TEXT, "lb_table" REGCLASS, "lbt_table" REGCLASS, "select" TEXT[] = '{*}', "where" TEXT = NULL, "default_trigger" BOOLEAN = FALSE)
+CREATE PROCEDURE public.create_user_view ("name" TEXT, "baserel" REGCLASS, "tranrel" REGCLASS)
     AS $$
 DECLARE
     -- имя будущей таблицы
-    "name"       CONSTANT TEXT NOT NULL   = COALESCE(public.format_table_name("name"), public.format_table_name("lb_table"::TEXT, 'v_'));
-    "columns"    CONSTANT TEXT[] NOT NULL = public.get_columns("lb_table");
-    "pk_columns" CONSTANT TEXT[]          = public.get_primary_key("lb_table");
+    "name"       CONSTANT TEXT NOT NULL   = COALESCE(public.format_table_name("name"), public.format_table_name("baserel"::TEXT, 'v_'));
+    "columns"    CONSTANT TEXT[] NOT NULL = public.get_columns("baserel");
+    "pk_columns" CONSTANT TEXT[]          = public.get_primary_key("baserel");
+    "select"              TEXT[] = '{}';
 BEGIN
     -- проверка, что таблицы заданы
-    IF ("lb_table" IS NULL) OR ("lbt_table" IS NULL) THEN
-        RAISE EXCEPTION USING MESSAGE = '"lb_table" and "lbt_table" cannot be NULL';
+    IF ("baserel" IS NULL) OR ("tranrel" IS NULL) THEN
+        RAISE EXCEPTION USING MESSAGE = '"baserel" and "tranrel" cannot be NULL';
     END IF;
     -- проверка, что pk_columns существуют
     IF ("pk_columns" IS NULL) THEN
-        RAISE EXCEPTION USING MESSAGE = '"lb_table" table must have primary keys';
+        RAISE EXCEPTION USING MESSAGE = '"baserel" table must have primary keys';
     END IF;
 
-    -- add default_lang in select
-    IF 'default_lang' = ANY ("columns") THEN
-        "select" = array_prepend('(b."default_lang" = bt."lang") IS TRUE AS "lang_is_default"'::TEXT, "select");
-    END IF;
-
-    -- set where
-    IF "where" IS NULL THEN
-        "where" = 'TRUE';
-    END IF;
+    -- добавить колонку lang_is_default
+    "select" = array_append('(b."default_lang" = bt."lang") IS TRUE AS "lang_is_default"'::TEXT, "select");
 
     -- create view
     -- USING — это сокращённая запись условия, полезная в ситуации, когда с обеих сторон соединения столбцы имеют одинаковые имена
@@ -36,26 +30,24 @@ BEGIN
         SELECT %2s
             FROM %3s b
             LEFT JOIN %4s bt USING (%5s)
-            WHERE %6s;
-    ', "name", array_to_string("select", ','), "lb_table", "lbt_table", array_to_string("pk_columns", ','), "where");
+            WHERE TRUE;
+    ', "name", array_to_string("select", ','), "baserel", "tranrel", array_to_string("pk_columns", ','));
 
-    IF "default_trigger" IS NULL THEN
-        -- создание triggers для редактиварония представления
-        -- %L - равнозначно вызову quote_nullable. Переводит данное значение в текстовый вид и заключает в апострофы
-        -- https://postgrespro.ru/docs/postgrespro/current/functions-string#FUNCTIONS-STRING-FORMAT
-        EXECUTE format('
-            CREATE TRIGGER "insert"
-                INSTEAD OF INSERT
-                ON %1s FOR EACH ROW
-            EXECUTE FUNCTION public.trigger_insert_user_view(%2L, %3L);
-        ', "name", "lb_table", "lbt_table");
-        EXECUTE format('
-            CREATE TRIGGER "update"
-                INSTEAD OF UPDATE
-                ON %1s FOR EACH ROW
-            EXECUTE FUNCTION public.trigger_update_user_view(%2L, %3L);
-        ', "name", "lb_table", "lbt_table");
-    END IF;
+    -- создание triggers для редактиварония представления
+    -- %L - равнозначно вызову quote_nullable. Переводит данное значение в текстовый вид и заключает в апострофы
+    -- https://postgrespro.ru/docs/postgrespro/current/functions-string#FUNCTIONS-STRING-FORMAT
+    -- EXECUTE format('
+    --     CREATE TRIGGER "insert"
+    --         INSTEAD OF INSERT
+    --         ON %1s FOR EACH ROW
+    --     EXECUTE FUNCTION public.trigger_insert_user_view(%2L, %3L);
+    -- ', "name", "baserel", "tranrel");
+    -- EXECUTE format('
+    --     CREATE TRIGGER "update"
+    --         INSTEAD OF UPDATE
+    --         ON %1s FOR EACH ROW
+    --     EXECUTE FUNCTION public.trigger_update_user_view(%2L, %3L);
+    -- ', "name", "baserel", "tranrel");
 END
 $$
 LANGUAGE plpgsql;
