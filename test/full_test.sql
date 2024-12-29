df526480911f4669396df6ea95ccf3541b235462
max_norin <79068976350@yandex.ru>
2024-12-29 14:22:47 +0300

    remove "

--- test/full_test.sql
+++ test/full_test.sql
@@ -1,5 +1,5 @@
 -- insert languages
-INSERT INTO "langs" ("language", "script", "region", "is_active", "title")
+INSERT INTO langs (language, script, region, is_active, title)
 VALUES ('ru', NULL, NULL, TRUE, 'Русский'),
        ('udm', NULL, NULL, TRUE, 'Удмурт'),
        ('en', NULL, 'US', TRUE, 'English'),
@@ -9,19 +9,19 @@ VALUES ('ru', NULL, NULL, TRUE, 'Русский'),
 CALL create_i18n_view(NULL::regclass, NULL::regclass);
 
 -- create i18n_words
-CREATE TABLE public."words"
+CREATE TABLE public.words
 (
-    "id"       SERIAL PRIMARY KEY,
-    "title"    VARCHAR(255) NOT NULL, -- default value
-    "original" VARCHAR(255)
-) INHERITS (public."untrans");
-CREATE TABLE public."word_trans"
+    id       SERIAL PRIMARY KEY,
+    title    VARCHAR(255) NOT NULL, -- default value
+    original VARCHAR(255)
+) INHERITS (public.untrans);
+CREATE TABLE public.word_trans
 (
-    "id"          INTEGER NOT NULL REFERENCES public."words" ("id") ON UPDATE CASCADE,
-    PRIMARY KEY ("id", "lang"),
-    "title"       VARCHAR(255), -- translation of "title" into language "lang"
-    "description" VARCHAR(255)
-) INHERITS (public."trans");
+    id          INTEGER NOT NULL REFERENCES public.words (id) ON UPDATE CASCADE,
+    PRIMARY KEY (id, lang),
+    title       VARCHAR(255), -- translation of title into language lang
+    description VARCHAR(255)
+) INHERITS (public.trans);
 CALL create_i18n_view('public.words'::REGCLASS, 'public.word_trans'::REGCLASS);
 
 -- # INSERT
