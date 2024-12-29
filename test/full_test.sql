ac04f67290a825fc624bf1ba132d8d79f1e2995a
max_norin <79068976350@yandex.ru>
2024-12-29 19:03:10 +0300

    update full_test ##

--- test/full_test.sql
+++ test/full_test.sql
@@ -24,25 +24,26 @@ CREATE TABLE public.word_trans
 ) INHERITS (public.trans);
 CALL create_i18n_view('public.words'::REGCLASS, 'public.word_trans'::REGCLASS);
 
+
 -- # INSERT
 
--- ok / INSERT (id, lang) = (DEFAULT, 'en-US')
+-- ## ok / INSERT (id, lang) = (DEFAULT, 'en-US')
 INSERT INTO public.i18n_words (is_tran, is_default_lang, lang, id, default_lang, original, title, description)
 VALUES (NULL, NULL, 'en-US', DEFAULT, NULL, 'te', 'tea', 'a drink made by pouring hot water onto')
 RETURNING *;
--- ok / INSERT (id, lang) = (100, 'en-US')
+-- ## ok / INSERT (id, lang) = (100, 'en-US')
 INSERT INTO public.i18n_words (is_tran, is_default_lang, lang, id, default_lang, original, title, description)
 VALUES (NULL, NULL, 'en-US', 100, NULL, 'te', 'tea', 'a drink made by pouring hot water onto')
 RETURNING *;
--- error / INSERT (id, lang) = (100, 'en-US') / Key (id)=(100) already exists.
+-- ## error / INSERT (id, lang) = (100, 'en-US') / Key (id)=(100) already exists.
 INSERT INTO public.i18n_words (is_tran, is_default_lang, lang, id, default_lang, original, title, description)
 VALUES (NULL, NULL, 'en-US', 100, NULL, 'te', 'tea', 'a drink made by pouring hot water onto')
 RETURNING *;
--- error / INSERT (id, lang) = (100, 'it') / Key (id)=(100) already exists.
+-- ## error / INSERT (id, lang) = (100, 'it') / Key (id)=(100) already exists.
 INSERT INTO public.i18n_words (is_tran, is_default_lang, lang, id, default_lang, original, title, description)
 VALUES (NULL, NULL, 'it', 100, NULL, 'te /it', 'tè', 'le foglie opportunamente trattate della pianta')
 RETURNING *;
--- ok / INSERT `untans`
+-- ## ok / INSERT `untans`
 INSERT INTO public.word_trans (lang, id, title, description)
 VALUES ('it', 100, 'tè', 'le foglie opportunamente trattate della pianta')
 RETURNING *;
@@ -53,14 +54,15 @@ RETURNING *;
     SELECT * FROM public.i18n_default_words WHERE id = 100;
 */
 
+
 -- # UPDATE
 
--- ok / UPDATE (title) WHERE (id) = (-100) / not exists
+-- ## ok / UPDATE (title) WHERE (id) = (-100) / not exists
 UPDATE public.i18n_words
 SET title = ('update (title) where (id) = (-100): ' || title)
 WHERE id = -100
 RETURNING *;
--- ok / UPDATE (title) WHERE (id) = (100) / update column of `trans` / will 4 rows in `untrans`
+-- ## ok / UPDATE (title) WHERE (id) = (100) / update column of `trans` / will 4 rows in `untrans`
 UPDATE public.i18n_words
 SET title = ('update (title) where (id) = (100): ' || title)
 WHERE id = 100
@@ -73,7 +75,7 @@ RETURNING *;
     # rollback
     DELETE FROM public.word_trans WHERE id = 100 AND lang IN ('ru', 'udm');
 */
--- ok / UPDATE (description) WHERE (id) = (100) / update namesake column of `trans` and `untrans` / will 4 rows in `untrans`
+-- ## ok / UPDATE (description) WHERE (id) = (100) / update namesake column of `trans` and `untrans` / will 4 rows in `untrans`
 UPDATE public.i18n_words
 SET description = ('update (description) where (id) = (100): ' || COALESCE(description, 'null'))
 WHERE id = 100
@@ -86,7 +88,7 @@ RETURNING *;
     # rollback
     DELETE FROM public.word_trans WHERE id = 100 AND lang IN ('ru', 'udm');
 */
--- ok / UPDATE (original) WHERE (id) = (100) / update column of `untrans` / will 4 rows in `untrans`
+-- ## ok / UPDATE (original) WHERE (id) = (100) / update column of `untrans` / will 4 rows in `untrans`
 -- TODO написать про эту ситуацию
 UPDATE public.i18n_words
 SET original = ('update (original)  where (id) = (100): ' || original)
@@ -100,20 +102,20 @@ RETURNING *;
     # rollback
     DELETE FROM public.word_trans WHERE id = 100 AND lang IN ('ru', 'udm');
 */
--- error / UPDATE (lang) WHERE (id, lang) = (100, 'en-US') / language change is not supported
+-- ## error / UPDATE (lang) WHERE (id, lang) = (100, 'en-US') / language change is not supported
 UPDATE public.i18n_words
 SET lang = 'ru'
 WHERE id = 100
   AND lang = 'en-US'
 RETURNING *;
--- error / UPDATE (id, lang) WHERE (id, lang) = (100, 'en-US') / language change is not supported
+-- ## error / UPDATE (id, lang) WHERE (id, lang) = (100, 'en-US') / language change is not supported
 UPDATE public.i18n_words
 SET id   = -100,
     lang = 'en'
 WHERE id = 100
   AND lang = 'en-US'
 RETURNING *;
--- ok / UPDATE (id) WHERE (id, lang) = (100, 'en-US') / id change
+-- ## ok / UPDATE (id) WHERE (id, lang) = (100, 'en-US') / id change
 UPDATE public.i18n_words
 SET id = -100
 WHERE id = 100
@@ -123,14 +125,13 @@ RETURNING *;
     # check
     SELECT * FROM public.i18n_words WHERE id = 100;
 */
--- ?????????????? / UPDATE (id) WHERE (id) = (100) / id change
+-- ## ?????????????? / UPDATE (id) WHERE (id) = (100) / id change
 -- TODO проблема
 UPDATE public.i18n_words
 SET id = 100
 WHERE id = -100
 RETURNING *;
--- ok / UPDATE (title, description) WHERE (id, lang) = (100, 'en-US') / update when there is record in `trans`
--- TODO остановился тут
+-- ## ok / UPDATE (title, description) WHERE (id, lang) = (100, 'en-US') / update when there is record in `trans`
 UPDATE public.i18n_words
 SET title       = ('update (title, description) where (id, lang) = (100, ''en-US''): ' || title),
     description = ('update (title, description) where (id, lang) = (100, ''en-US''): ' || description)
@@ -143,7 +144,7 @@ RETURNING *;
     FROM public.word_trans
     WHERE id = 100;
 */
--- ok / UPDATE (title, description) WHERE (id, lang) = (100, 'ru') / update when there is not record in `trans`
+-- ## ok / UPDATE (title, description) WHERE (id, lang) = (100, 'ru') / update when there is not record in `trans`
 UPDATE public.i18n_words
 SET title       = ('update (title, description) where (id, lang) = (100, ''ru''): ' || COALESCE(title, 'null')),
     description = ('update (title, description) where (id, lang) = (100, ''ru''): ' || COALESCE(description, 'null'))
@@ -156,7 +157,7 @@ RETURNING *;
     FROM public.word_trans
     WHERE id = 100;
 */
--- ok / UPDATE (is_tran, is_default_lang) WHERE (id, lang) = (100, 'ru') / not edit
+-- ## ok / UPDATE (is_tran, is_default_lang) WHERE (id, lang) = (100, 'ru') / not edit
 -- TODO проблема
 UPDATE public.i18n_words
 SET is_tran         = FALSE, -- no edit
@@ -170,7 +171,7 @@ RETURNING *;
     FROM public.i18n_words
     WHERE id = 100;
 */
--- ok / UPDATE (default_lang) WHERE (id) = (100)
+-- ## ok / UPDATE (default_lang) WHERE (id) = (100)
 UPDATE public.i18n_words
 SET default_lang = 'en-US'
 WHERE id = 100
@@ -186,22 +187,22 @@ RETURNING *;
 -- # DROP
 
 -- 1.
--- ok
+-- ## ok
 DROP VIEW public.i18n_words;
--- error / not exists
+-- ## error / not exists
 DROP FUNCTION public.i18n_words__insert();
--- error / not exists
+-- ## error / not exists
 DROP FUNCTION public.i18n_words__update();
--- ok
+-- ## ok
 DROP VIEW public.i18n_default_words;
 -- 2.
--- create i18n_words
+-- ## create i18n_words
 CALL create_i18n_view('public.words'::REGCLASS, 'public.word_trans'::REGCLASS);
--- ok
+-- ## ok
 DROP TABLE public.words CASCADE;
--- error / not exists
+-- ## error / not exists
 DROP FUNCTION public.i18n_words__insert();
--- error / not exists
+-- ## error / not exists
 DROP FUNCTION public.i18n_words__update();
--- ok
+-- ## ok
 DROP TABLE public.word_trans CASCADE;
