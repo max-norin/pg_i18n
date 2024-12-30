bc553427d0e25e3473b7e6855afefbd341550cd3
max_norin <79068976350@yandex.ru>
2024-12-30 15:06:49 +0300

    fix EVENT TRIGGER

--- dist/pg_i18n--2.0--2.1.sql
+++ dist/pg_i18n--2.0--2.1.sql
@@ -107,3 +107,10 @@ END;
 $$
 LANGUAGE plpgsql
 VOLATILE;
+/*
+=================== INIT ===================
+*/
+DROP EVENT TRIGGER "drop_i18n_triggers";
+CREATE EVENT TRIGGER "drop_i18n_triggers" ON sql_drop
+    WHEN TAG IN ('DROP TABLE', 'DROP VIEW')
+EXECUTE PROCEDURE public.event_trigger_drop_i18n_triggers ();
--- event-triggers/init.sql
+++ event-triggers/init.sql
@@ -1,3 +1,3 @@
 CREATE EVENT TRIGGER "drop_i18n_triggers" ON sql_drop
-    WHEN TAG IN ('DROP VIEW')
+    WHEN TAG IN ('DROP TABLE', 'DROP VIEW')
 EXECUTE PROCEDURE public.event_trigger_drop_i18n_triggers ();
