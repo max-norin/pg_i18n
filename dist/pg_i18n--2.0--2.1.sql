01c56ac693fdf3246465c21777b5ba237f7ef915
max_norin <79068976350@yandex.ru>
2024-12-30 22:47:09 +0300

    fix lang (TEXT)

--- dist/pg_i18n--2.0--2.1.sql
+++ dist/pg_i18n--2.0--2.1.sql
@@ -6,6 +6,30 @@ ALTER FUNCTION @extschema@.language_rule(TEXT) RENAME TO language;
 ALTER FUNCTION @extschema@.region_rule(TEXT) RENAME TO region;
 ALTER FUNCTION @extschema@.script_rule(TEXT) RENAME TO script;
 /*
+=================== LANG ===================
+*/
+CREATE OR REPLACE FUNCTION @extschema@.lang ("value" TEXT)
+    RETURNS BOOLEAN
+    AS $$
+DECLARE
+    "arr"          CONSTANT TEXT[]  = string_to_array("value", '-');
+    "length"       CONSTANT INT     = array_length("arr", 1);
+    "has_language" CONSTANT BOOLEAN = "arr"[1] IS NOT NULL;
+    "has_script"   CONSTANT BOOLEAN = "arr"[2] IS NOT NULL;
+    "has_region"   CONSTANT BOOLEAN = "arr"[3] IS NOT NULL;
+BEGIN
+    IF ("length" IS NULL OR "length" > 3) THEN
+        RETURN FALSE;
+    END IF;
+    RETURN ("has_language" AND @extschema@.language("arr"[1])) AND
+           (NOT ("has_script") OR (@extschema@.script("arr"[2]) OR (@extschema@.region("arr"[2]) AND NOT "has_region"))) AND
+           (NOT ("has_region") OR @extschema@.region("arr"[3]));
+END
+$$
+LANGUAGE plpgsql
+IMMUTABLE
+RETURNS NULL ON NULL INPUT;
+/*
 =================== GET_COLUMNS ===================
 */
 DROP FUNCTION @extschema@.get_columns (OID, BOOLEAN, TEXT);
