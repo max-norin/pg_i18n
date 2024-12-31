de894387ab179de16834d2d3b0ef752c973ec0c8
max_norin <79068976350@yandex.ru>
2024-12-31 15:23:59 +0300

    update dev

--- event-triggers/init.sql
+++ event-triggers/init.sql
@@ -1,3 +1,4 @@
+-- DROP EVENT TRIGGER "drop_i18n_triggers";
 CREATE EVENT TRIGGER "drop_i18n_triggers" ON sql_drop
     WHEN TAG IN ('DROP TABLE', 'DROP VIEW')
 EXECUTE PROCEDURE public.event_trigger_drop_i18n_triggers ();
